1b2691b2837ca5dd07b916b8530eee692710b597
Gabriel Ayuel <ayuelgarang105@gmail.com>
2021-04-09 15:36:50 +0300

    initial commit

---
+++ js-folder/scripts.js
@@ -0,0 +1,6 @@
+var add = function(number1, number2) {
+		return (number1 + number2);
+          };
+var number1 = parseInt(prompt("Enter the number: "));
+var number2 = parseInt(prompt("Enter the number: "));
+	alert(add(number1, number2));
