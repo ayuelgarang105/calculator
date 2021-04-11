5a8312abe7dc8837ddff171209d715f8445f8bb9
Gabriel Ayuel <ayuelgarang105@gmail.com>
2021-04-11 14:05:06 +0300

    Change multiplication to addition

--- js-folder/scripts.js
+++ js-folder/scripts.js
@@ -1,6 +1,22 @@
 var add = function(number1, number2) {
-		return (number1 + number2);
+	return (number1 + number2);
           };
+
+var subtract = function(number1, number2) {
+	return (number1 - number2);
+	  };
+
+
+var multiply = function(number1, number2) {
+	return (number1 * number2);
+	  };
+
+var divide = function(number1, number2) {
+	return (number1 / number2);
+	  };
+
 var number1 = parseInt(prompt("Enter the number: "));
 var number2 = parseInt(prompt("Enter the number: "));
-	alert(add(number1, number2));
+
+var results = add(number1, number2);
+alert(results);
